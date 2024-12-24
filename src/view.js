6bc55d3a446e50b249b2c93735330dd408866343
imhojeong <39ghwjd@naver.com>
2024-12-24 22:28:56 +0700

    refactor: refactor views.js

--- src/view.js
+++ src/view.js
@@ -1,34 +1,3 @@
-const getTodoElement = todo => {
-    const {
-      text,
-      completed
-    } = todo
-  
-    return `
-    <li ${completed ? 'class="completed"' : ''}>
-      <div class="view">
-        <input 
-          ${completed ? 'checked' : ''}
-          class="toggle" 
-          type="checkbox">
-        <label>${text}</label>
-        <button class="destroy"></button>
-      </div>
-      <input class="edit" value="${text}">
-    </li>`
-  }
-  
-  const getTodoCount = todos => {
-    const notCompleted = todos
-      .filter(todo => !todo.completed)
-  
-    const { length } = notCompleted
-    if (length === 1) {
-      return '1 Item left'
-    }
-  
-    return `${length} Items left`
-  }
 
   /**
    * 기본으로 사용되는 타깃 DOM 요소를 받음 
@@ -36,11 +5,6 @@ const getTodoElement = todo => {
    * !! DOM 수정은 가상임!을 명심 (분리된 요소로 작업)
    */
   export default (targetElement, state) => {
-    const {
-      currentFilter,
-      todos
-    } = state
-
 
     // 분리된 요소를 생성하고자, cloneNode 메서드를 사용해 기존 노드를 복제
     // 새로 생성된 이 DOM 요소는 실제 DOM 요소
@@ -50,19 +14,10 @@ const getTodoElement = todo => {
     const list = element.querySelector('.todo-list')
     const counter = element.querySelector('.todo-count')
     const filters = element.querySelector('.filters')
-  
-    list.innerHTML = todos.map(getTodoElement).join('')
-    counter.textContent = getTodoCount(todos)
-  
-    Array
-      .from(filters.querySelectorAll('li a'))
-      .forEach(a => {
-        if (a.textContent === currentFilter) {
-          a.classList.add('selected')
-        } else {
-          a.classList.remove('selected')
-        }
-      })
+
+    list.replaceWith(todosView(list, state));
+    counter.replaceWith(counterView(list, state));
+    filters.replaceWith(filtersView(list, state));
   
     return element
   }
