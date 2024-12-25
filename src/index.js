e77e3866e8bf466a827d5e0c8d167c7e9a3be88a
imhojeong <39ghwjd@naver.com>
2024-12-25 11:28:33 +0700

    add: add dynamic-rendering

--- src/index.js
+++ src/index.js
@@ -14,8 +14,20 @@ const state = {
   currentFilter: 'All'
 }
 
-window.requestAnimationFrame(() => {
-    const main = document.querySelector('.todoapp')
-    const newMain = registry.renderRoot(main, state)
-    main.replaceWith(newMain)
-})
+// 이 줄부터 끝까지...
+// 새 데이터가 있을 때마다 가상 루트 요소를 만든 다음 실제 요소를 새로 생성된 요소로 변경
+// 소규모 애플리케이션에서는 충분한 성능을 발휘하지만, 대규모 프로젝트에선 성능을 저하시킬 수 있음
+const render = () => {
+    window.requestAnimationFrame(()=>{
+        const main = document.querySelector('.todoapp')
+        const newMain = registry.renderRoot(main, state)
+        main.replaceWith(newMain)
+    })
+}
+
+window.setInterval(()=> {
+    state.todos = getTodos()
+    render()
+}, 5000)
+
+render();
